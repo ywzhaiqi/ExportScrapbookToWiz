6566916f7fbdc23c464ad8ed85e64cf100bd7578
ywzhaiqi <ywzhaiqi@gmail.com>
2014-07-19 16:33:28 +0800

    1.3 增加 debug 设置，添加主页、更新链接

--- README.md
+++ README.md
@@ -3,16 +3,21 @@
 
 由于 Wiz 自带的保存比较弱，而 Scrapbook 保存很强大，所以就想结合起来。本扩展从 Scrapbook 导出单文件到 Wiz，包括样式、图片等资源。
 
- - 需要有 WizBrother Wiz Plugin 2.0 扩展。
+ - 需要有 **WizBrother Wiz Plugin 2.0 扩展**（Wiz 客户端自带的 wiz.xpi），[26 楼](http://bbs.kafan.cn/forum.php?mod=redirect&goto=findpost&ptid=1756913&pid=32096428) 有个上传文件。
  - 默认忽略 .ttf 结尾的字体文件。在某些站点 Scrapbook 会保存字体，会让导出到 Wiz 的文件偏大。
+ - 如果要自动更新，需要保证 github 网络的顺畅。
+
+还有一种方法
+
+ - 用 Scrapbook 的 dom橡皮擦 把无用的信息清除，然后用wiz保存完整页面，即正文
 
 ### 地址及镜像
 
- - [ExportScrapbookToWiz :: Firefox 附加组件](https://addons.mozilla.org/zh-cn/firefox/addon/exportscrapbooktowiz/)
+ - [ExportScrapbookToWiz :: Firefox 附加组件](https://addons.mozilla.org/zh-cn/firefox/addon/exportscrapbooktowiz/)，审核失败，链接已失效。由于 Wiz 并未将 2.0 扩展上传 AMO，故本扩展无法测试通过。
  - [github](https://github.com/ywzhaiqi/ExportScrapbookToWiz)，[安装包地址](https://github.com/ywzhaiqi/ExportScrapbookToWiz/releases)
  - [卡饭论坛](http://bbs.kafan.cn/thread-1756913-1-1.html)
  - [Mozilla 社区](http://g.mozest.com/thread-44774-1-1)
 
-![menu.png](menu.png)
+![menu.png](img/menu.png)
 
-![export.png](export.png)
+![export.png](img/export.png)
--- src/chrome/content/scrapbook-sidebar.js
+++ src/chrome/content/scrapbook-sidebar.js
@@ -1,92 +1,110 @@
-Components.utils.import("resource://gre/modules/devtools/Console.jsm")
+var { classes: Cc, interfaces: Ci, utils: Cu } = Components;
+Cu.import("resource://gre/modules/devtools/Console.jsm")
 
-var mainWindow = window.QueryInterface(Components.interfaces.nsIInterfaceRequestor)
-                     .getInterface(Components.interfaces.nsIWebNavigation)
-                     .QueryInterface(Components.interfaces.nsIDocShellTreeItem)
-                     .rootTreeItem
-                     .QueryInterface(Components.interfaces.nsIInterfaceRequestor)
-                     .getInterface(Components.interfaces.nsIDOMWindow);
+if (typeof ExportScrapbookToWiz === 'undefined') {
+  var ExportScrapbookToWiz = {};
+}
 
-var ExportToWiz = {
-    export: function() {
-        var aRes = sbTreeUI.resource;
-        if (!aRes)
-          return;
+ExportScrapbookToWiz = {
+  debug: false,
 
-        var id = ScrapBookData.getProperty(aRes, "id"),
-          type = ScrapBookData.getProperty(aRes, "type"),
-          title = ScrapBookData.getProperty(aRes, "title"),
-          url = ScrapBookData.getProperty(aRes, "source");
+  mainWindow: window.QueryInterface(Ci.nsIInterfaceRequestor)
+    .getInterface(Ci.nsIWebNavigation)
+    .QueryInterface(Ci.nsIDocShellTreeItem)
+    .rootTreeItem
+    .QueryInterface(Ci.nsIInterfaceRequestor)
+    .getInterface(Ci.nsIDOMWindow),
 
-        // 当 type 为 site 时，有多个 html，会有问题
+  get prefs() {
+      delete this.prefs;
+      return this.prefs = Cc["@mozilla.org/preferences-service;1"]
+                      .getService(Ci.nsIPrefService)
+                      .getBranch("extensions.exportscrapbooktowiz.");
+  },
 
-        var aFolder = ScrapBookUtils.getContentDir(id),
-          indexPath = aFolder.path + '\\index.html',
-          preUrl = url.replace(/[^\/]*$/, '');
+  export: function() {
+    var aRes = sbTreeUI.resource;
+    if (!aRes)
+      return;
 
-        var contentConfig = "[Common]\r\nURL=" + url +
-              "\r\nTitle=" + title +
-              "\r\nFileNameAll=" + indexPath +
-              "\r\nFileNameSel=" + indexPath +
-              "\r\n[Resources]";
+    this.debug = this.prefs.getBoolPref('debug');
 
-        var resourceFilesIndex = 0;
+    var id = ScrapBookData.getProperty(aRes, "id"),
+      type = ScrapBookData.getProperty(aRes, "type"),
+      title = ScrapBookData.getProperty(aRes, "title"),
+      url = ScrapBookData.getProperty(aRes, "source");
 
-        // 罗列该目录下的所有文件
-        let files = aFolder.directoryEntries.QueryInterface(Ci.nsISimpleEnumerator);
-        while (files.hasMoreElements()) {
-            let file = files.getNext().QueryInterface(Ci.nsIFile),
-              filename = file.leafName;
-            if (filename == 'index.html' || filename == 'index.dat' || filename.endsWith('.ttf'))
-              continue;
+    // 当 type 为 site 时，有多个 html，会有问题
 
-            contentConfig += "\r\n" + resourceFilesIndex + "_URL=" + preUrl + filename;
-            contentConfig += "\r\n" + resourceFilesIndex + "_File=" + file.path;
-            resourceFilesIndex += 1;
-        }
+    var aFolder = ScrapBookUtils.getContentDir(id),
+      indexPath = aFolder.path + '\\index.html',
+      preUrl = url.replace(/[^\/]*$/, '');
 
-        contentConfig += "\r\nCount=" + resourceFilesIndex;
+    var contentConfig = "[Common]\r\nURL=" + url +
+      "\r\nTitle=" + title +
+      "\r\nFileNameAll=" + indexPath +
+      "\r\nFileNameSel=" + indexPath +
+      "\r\n[Resources]";
 
-        // console.log(contentConfig);
-        console.log('111', type)
-        
-        this.launchWiz(contentConfig);
-    },
-    launchWiz: function(contentConfig) {
-      var wiz_km_writeFileWithCharset = mainWindow.wiz_km_writeFileWithCharset,
-        wiz_km_getWizAppPath = mainWindow.wiz_km_getWizAppPath,
-        wiz_km_unicodeToBytes = mainWindow.wiz_km_unicodeToBytes,
-        wiz_km_base64Encode = mainWindow.wiz_km_base64Encode,
-        wiz_km_runExeFile = mainWindow.wiz_km_runExeFile;
+    var resourceFilesIndex = 0;
 
-      var tmpDir = Components.classes["@mozilla.org/file/directory_service;1"]
-                          .getService(Components.interfaces.nsIProperties)
-                          .get("TmpD", Components.interfaces.nsIFile);
+    // 罗列该目录下的所有文件
+    let files = aFolder.directoryEntries.QueryInterface(Ci.nsISimpleEnumerator);
+    while (files.hasMoreElements()) {
+      let file = files.getNext().QueryInterface(Ci.nsIFile),
+        filename = file.leafName;
+      if (filename == 'index.html' || filename == 'index.dat' || filename.endsWith('.ttf'))
+        continue;
 
-      var fileNameConfig = tmpDir.clone();
-      fileNameConfig.append("wiz_km_firefox.ini");
-
-      wiz_km_writeFileWithCharset(fileNameConfig, contentConfig, "utf-8");
-
-      var fileNameExe = wiz_km_getWizAppPath() + "Wiz.exe";
-
-      var exeFile = Components.classes["@mozilla.org/file/local;1"].createInstance(Components.interfaces.nsILocalFile);
-      exeFile.initWithPath(fileNameExe);
-
-      var dllFileName = wiz_km_getWizAppPath() + "NPWizWebCapture.dll";
-      var functionName = "WizKMResourceToDocument";
-
-      var params = fileNameConfig.path;
-      params = wiz_km_unicodeToBytes(params, "utf-8");
-      params = wiz_km_base64Encode(params);
-      params = "/FileName=" + params;
-      params = params.replace(/\r/gi, "");
-      params = params.replace(/\n/gi, "");
-
-      var firefoxType = "/firefox=1";
-
-      var cmdLineExe = [dllFileName, functionName, params, firefoxType];
-
-      wiz_km_runExeFile(exeFile, cmdLineExe, false);
+      contentConfig += "\r\n" + resourceFilesIndex + "_URL=" + preUrl + filename;
+      contentConfig += "\r\n" + resourceFilesIndex + "_File=" + file.path;
+      resourceFilesIndex += 1;
     }
+
+    contentConfig += "\r\nCount=" + resourceFilesIndex;
+
+    if (this.debug) {
+      console.log('type is ', type);
+      console.log('contentConfig is ', contentConfig);
+    }
+
+    this.launchWiz(contentConfig);
+  },
+  launchWiz: function(contentConfig) {
+    var wiz_km_writeFileWithCharset = this.mainWindow.wiz_km_writeFileWithCharset,
+      wiz_km_getWizAppPath = this.mainWindow.wiz_km_getWizAppPath,
+      wiz_km_unicodeToBytes = this.mainWindow.wiz_km_unicodeToBytes,
+      wiz_km_base64Encode = this.mainWindow.wiz_km_base64Encode,
+      wiz_km_runExeFile = this.mainWindow.wiz_km_runExeFile;
+
+    var tmpDir = Cc["@mozilla.org/file/directory_service;1"]
+      .getService(Ci.nsIProperties)
+      .get("TmpD", Ci.nsIFile);
+
+    var fileNameConfig = tmpDir.clone();
+    fileNameConfig.append("wiz_km_firefox.ini");
+
+    wiz_km_writeFileWithCharset(fileNameConfig, contentConfig, "utf-8");
+
+    var fileNameExe = wiz_km_getWizAppPath() + "Wiz.exe";
+
+    var exeFile = Cc["@mozilla.org/file/local;1"].createInstance(Ci.nsILocalFile);
+    exeFile.initWithPath(fileNameExe);
+
+    var dllFileName = wiz_km_getWizAppPath() + "NPWizWebCapture.dll";
+    var functionName = "WizKMResourceToDocument";
+
+    var params = fileNameConfig.path;
+    params = wiz_km_unicodeToBytes(params, "utf-8");
+    params = wiz_km_base64Encode(params);
+    params = "/FileName=" + params;
+    params = params.replace(/\r/gi, "");
+    params = params.replace(/\n/gi, "");
+
+    var firefoxType = "/firefox=1";
+
+    var cmdLineExe = [dllFileName, functionName, params, firefoxType];
+
+    wiz_km_runExeFile(exeFile, cmdLineExe, false);
+  }
 };
